09e43219ceb0b5fbceb6721ecba53e22aa3be7cb
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2023-01-29 00:27:47 -0300

    [FEATURE]: Add base styles to TopNotes component

--- components/TopNotes/index.tsx
+++ components/TopNotes/index.tsx
@@ -1,4 +1,6 @@
 import Section from "../../layouts/Section.tsx";
+import Body from "../../theme/typography/Body.tsx";
+import Pinned from "../../icons/Pinned.tsx";
 
 const notesMock = [
   {
@@ -16,44 +18,34 @@ const notesMock = [
     description: "Some description for note",
     href: "/notes/some",
   },
-  {
-    title: "Note 4",
-    description: "Some description for note",
-    href: "/notes/some",
-  },
 ];
 
 interface ICardProps {
   href: string;
   title: string;
-  description?: string;
 }
 
 function Card(props: ICardProps) {
   return (
-    <a href={props.href} class="border-1 border-blue-100">
-      <p>
-        {props.title}
-      </p>
-      <p>
-        {props.description}
-      </p>
+    <a
+      href={props.href}
+      class="border-1 border-blue-100 p-3 flex items-center gap-2"
+    >
+      <Pinned width={20} height={20} />
+      <Body text={props.title} variant="medium" />
     </a>
   );
 }
 
 export default function TopNotes() {
   return (
-    <Section title="Pinned notes">
-      <div class="grid gap-7 grid-cols-4">
-        {notesMock.map((note) => (
-          <Card
-            href={note.href}
-            title={note.title}
-            description={note.description}
-          />
-        ))}
-      </div>
+    <Section title="Pinned notes" class="grid gap-7 grid-cols-3">
+      {notesMock.map((note) => (
+        <Card
+          href={note.href}
+          title={note.title}
+        />
+      ))}
     </Section>
   );
 }
