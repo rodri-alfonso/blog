0c97998a23fad7c14300992a8a7c831e41be906c
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2023-02-12 15:48:14 -0300

    [FEATURE]: Create NavMenu

---
+++ components/NavMenu.tsx
@@ -0,0 +1,17 @@
+import Link from "./Link.tsx";
+import { HEADER_LINKS } from "../config.ts";
+
+export default function Header({ currentPath }: { currentPath: string }) {
+  return (
+    <nav class="flex gap-8 p-2 mr-4">
+      {HEADER_LINKS.map((link) => (
+        <Link
+          key={link.title}
+          href={link.href}
+          title={link.title}
+          isActive={link.href === currentPath}
+        />
+      ))}
+    </nav>
+  );
+}
