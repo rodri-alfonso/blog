415eadf6af981928b8b8dcf8d5d609930e4d184e
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2023-02-17 00:07:38 -0300

    [UPDATE]: Improve Header styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,34 @@
-import Link from "./Link.tsx";
 import { HEADER_LINKS } from "../config.ts";
+import { JSX } from "preact";
+import Body from "../theme/typography/Body.tsx";
+import ThemeSwitcher from "../islands/ThemeSwitcher.tsx";
+import Avatar from "../icons/Avatar.tsx";
+
+interface LinkProps extends JSX.HTMLAttributes<HTMLAnchorElement> {
+  title: string;
+  isActive: boolean;
+}
 
 export default function Header({ currentPath }: { currentPath: string }) {
+  function Link(props: LinkProps) {
+    const { title, href, isActive, ...rest } = props;
+
+    return (
+      <a
+        class={"py-1 px-2.5 rounded-lg text-gray-400 hover:text-gray-900 active:scale-95 transition-all " +
+          (props.isActive ? " text-gray-900 bg-gray-200 " : "")}
+        href={props.href}
+        {...rest}
+      >
+        <Body text={props.title} variant="medium" />
+      </a>
+    );
+  }
+
   return (
     <header class="flex justify-between items-center">
-      <nav class="flex gap-8">
+      <Avatar width={40} height={40} background="bg-gray-100" />
+      <nav class="flex gap-4">
         {HEADER_LINKS.map((link) => (
           <Link
             key={link.title}
@@ -14,7 +38,7 @@ export default function Header({ currentPath }: { currentPath: string }) {
           />
         ))}
       </nav>
-      <div class="w-5 h-5 bg-gray-700 rounded" />
+      <ThemeSwitcher />
     </header>
   );
 }
