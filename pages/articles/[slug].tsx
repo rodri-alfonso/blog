be89cf9e7342c8dddf0fd440a8285e32abeb49a6
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2023-04-26 18:28:06 -0300

    [FEATURE] Create pages/article page and mock articles

---
+++ pages/articles/[slug].tsx
@@ -0,0 +1,35 @@
+import { getPartialPosts, getPost } from '@/utils'
+import { MDXRemote } from 'next-mdx-remote'
+import { serialize } from 'next-mdx-remote/serialize'
+
+function Post({ data, content }) {
+	return (
+		<div>
+			<h1>{data.title}</h1>
+			<time>{data.date}</time>
+			<MDXRemote {...content} />
+		</div>
+	)
+}
+
+export default Post
+
+export const getStaticPaths = async () => {
+	const posts = getPartialPosts()
+	const paths = posts.map((post) => ({ params: { slug: post.slug } }))
+	return {
+		paths,
+		fallback: false,
+	}
+}
+
+export const getStaticProps = async ({ params }) => {
+	const post = getPost(params.slug)
+	const mdxSource = await serialize(post.content)
+	return {
+		props: {
+			data: post.data,
+			content: mdxSource,
+		},
+	}
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,31 @@
+import { getPost, getPartialPosts } from '@/utils'
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({ posts }: any) {
 	return (
 		<main>
-			<p className={inter.className}></p>
+			<ul>
+				<li>
+					{posts.map((post, index) => (
+						<Link key={index} href={`/articles/${post.slug}`}>
+							<h6>{post.data.title}</h6>
+						</Link>
+					))}
+				</li>
+			</ul>
 		</main>
 	)
+}
+
+export const getStaticProps = () => {
+	const posts = getPartialPosts()
+
+	return {
+		props: {
+			posts,
+		},
+	}
 }
