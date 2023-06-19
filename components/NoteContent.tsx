e38170b76c0d68ea52212e7abc42bef3e06a634e
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2023-06-18 21:46:25 -0300

    [UPDATE] Add NoteContent mock

--- components/NoteContent.tsx
+++ components/NoteContent.tsx
@@ -6,8 +6,16 @@ interface Props {
 
 export default function NoteContent({ title, description, topic }: Props) {
 	return (
-		<article>
-			<p>{description}</p>
+		<article className=' grid gap-6'>
+			<div className='flex gap-4 items-center'>
+				<img src='/avatar.svg' alt='icon' className='w-10 h-10' />
+
+				<div className=''>
+					<h2 className='text-lg font-medium -mb-1'>{title}</h2>
+					<p className='text-gray-500'>{description}</p>
+				</div>
+			</div>
+			<div className='bg-gray-200 rounded-lg h-32' />
 		</article>
 	)
 }
--- pages/notes/index.tsx
+++ pages/notes/index.tsx
@@ -1,6 +1,9 @@
 import Page from '@layouts/Page'
 import Banner from '@components/Banner'
 import NoteSelector from '@components/NoteSelector'
+import NoteContent from '@components/NoteContent'
+
+const MOCK = [{ title: 'Note title mock', description: 'This is a great description of whats supouse to be this note' }]
 
 export default function Notes() {
 	return (
@@ -8,6 +11,11 @@ export default function Notes() {
 			<Banner title='What I&#39;ve read' description={`A few notes about CSS, web performance, and news`} />
 			<div className='w-full mx-auto'>
 				<NoteSelector />
+			</div>
+			<div className='pt-4 grid gap-3'>
+				{MOCK.map((note) => (
+					<NoteContent description={note.description} title={note.title} topic='language' key={note.title} />
+				))}
 			</div>
 		</Page>
 	)
