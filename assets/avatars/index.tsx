2ffe4d1f4fe93162c7ad45763232751024775ed9
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2023-07-04 22:35:57 -0300

    [FEATURE] Add avatars

--- assets/avatars/index.tsx
+++ assets/avatars/index.tsx
@@ -1,17 +1,20 @@
 import { lazy, Suspense } from 'react'
 
+export type AvatarSizes = 50 | 126
+
 interface Props {
 	type: Avatar
+	size?: AvatarSizes
 }
 
-type Avatar = 'default' | 'glasses' | 'earphones' | 'classic'
+type Avatar = 'default' | 'earpods' | 'cute' | 'glasses'
 
-export default function AvatarOrchester({ type }: Props) {
+export default function AvatarOrchester({ type, size }: Props) {
 	const IconComponent = lazy(() => import(`./${type}`))
 
 	return (
 		<Suspense>
-			<IconComponent />
+			<IconComponent size={size} />
 		</Suspense>
 	)
 }
