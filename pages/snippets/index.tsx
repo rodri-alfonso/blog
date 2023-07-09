3991fd5fe5ab25ef1b06f1e516730ebcba3b2310
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2023-07-09 13:45:33 -0300

    [UPDATE] Improve Collapsable components and create snippets

--- pages/snippets/index.tsx
+++ pages/snippets/index.tsx
@@ -1,30 +1,42 @@
 import Page from '@layouts/Page'
 import Banner from '@components/Banner'
 import Collapsable from '@components/Collapsable'
+import { getSnippets } from '@utils/content'
+import { MDXRemote } from 'next-mdx-remote'
+import { CH } from '@code-hike/mdx/components'
 
-const MOCK = [
-	{
-		title: 'Get the current date',
-	},
-	{
-		title: 'Get the random item from an array',
-	},
-]
-
-export default function Snippets() {
+export default function Snippets({ snippets }: any) {
 	return (
 		<Page description='' title="Rodrigo's Blog | Snippets">
-			<Banner title='What I&#39;ve used' description={`A collection with 2 JavaScript snippets that I used most`} />
+			<Banner
+				title='What I&#39;ve used'
+				description={`A collection with ${snippets.length} JavaScript snippets that I used most`}
+			/>
 			<section className='pt-4 grid gap-4'>
 				<h1 className='text-xl font-medium pl-4'>One line utilities</h1>
-				<div>
-					{MOCK.map((item) => (
-						<Collapsable key={item.title} label={item.title}>
-							{item.title}
+				<div className='grid gap-0.5 text-gray-600 hover:text-gray-400 font-medium first-letter:capitalize'>
+					{snippets.map((snippet: any, index: number) => (
+						<Collapsable
+							key={snippet.data.title}
+							label={snippet.data.title}
+							tag={snippet.data.topic}
+							open={index === 0}
+						>
+							<MDXRemote components={{ CH }} {...snippet.content} />
 						</Collapsable>
 					))}
 				</div>
 			</section>
 		</Page>
 	)
+}
+
+export const getStaticProps = async () => {
+	const snippets = await getSnippets()
+
+	return {
+		props: {
+			snippets,
+		},
+	}
 }
--- utils/content.ts
+++ utils/content.ts
@@ -2,6 +2,19 @@ import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
 import { PartialContent, Content, PartialConfig, Config } from '@content/types'
+import { serialize } from 'next-mdx-remote/serialize'
+import { remarkCodeHike } from '@code-hike/mdx'
+
+export const getMdxSource = async (content: string) => {
+	return await serialize(content, {
+		mdxOptions: {
+			remarkPlugins: [
+				[remarkCodeHike, { autoImport: false, theme: 'one-dark-pro', showCopyButton: true, lineNumbers: true }],
+			],
+			useDynamicImport: true,
+		},
+	})
+}
 
 export const getContent = ({ slug, type }: Config): Content => {
 	const fileContents = fs.readFileSync(path.join(`content/${type}/${slug}.mdx`), 'utf8')
@@ -28,4 +41,22 @@ export const getPartialContent = ({ type }: PartialConfig): PartialContent[] => 
 	})
 
 	return allContentData.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+}
+
+export const getSnippets = async () => {
+	const promises = []
+
+	for await (const entry of fs.opendirSync('content/snippets')) {
+		const slug = entry.name.replace('.mdx', '')
+
+		const fileContents = fs.readFileSync(path.join(`content/snippets/${slug}.mdx`), 'utf8')
+		const { data, content } = matter(fileContents)
+
+		const parsedMdx = await getMdxSource(content)
+
+		promises.push({ data, content: parsedMdx })
+	}
+
+	const snippets = await Promise.all(promises)
+	return snippets
 }
